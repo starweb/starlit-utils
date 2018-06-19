13f0ed576ffad5c02bbb5ae786fbf212e6248c86
Patrick Rodacker <patrick@rodacker.de>
2018-06-19 07:15:52 +0200

    updates deprecation notice

--- src/Arr.php
+++ src/Arr.php
@@ -214,7 +214,7 @@ class Arr
      * @param mixed  $default
      * @return mixed
      *
-     * @deprecated will be removed in v1.0.0 which required php7.2 and thus can be replaced by using the null coalescing operator
+     * @deprecated will be removed in v1.0.0 which requires php7.2 and thus can be replaced by using the null coalescing operator
      */
     public static function getValue(array $array, $key, $default = null)
     {
